77ba21bac628228b9f8c0a8d07ebb73fb4924e1c
xTobu <junxiang.oriint@gmail.com>
2018-07-21 09:25:40 +0800

    wip

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import qs from 'qs';
 export default {
+    // 取得餐廳名單
     getRestaurantList(context) {
         axios.get('https://easy-mock.com/mock/5b45cffb43b7ef2c8bf92b96/RestaurantsList')
             .then((response) => {
